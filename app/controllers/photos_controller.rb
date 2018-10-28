af32c5221b93331eb070db91743321e057cf48a0
Michael Fich <michael@michaelfich.com>
2018-10-28 16:57:34 -0400

    Setup the photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,16 @@
 class PhotosController < ApplicationController
+  def ankita
+    @name = 'ankita'
+    @count = 13
+    render :show
+  end
+
+  def hamza
+    @name = 'hamza'
+    @count = 9
+    render :show
+  end
+
   def suri
     @name = 'suri'
     @count = 9
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'ankita' => 'photos#ankita'
+  get 'hamza' => 'photos#hamza'
   get 'suri' => 'photos#suri'
 end
