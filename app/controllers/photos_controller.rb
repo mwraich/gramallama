8643a49354a51bff8333b861f07e1190ecbe2ae5
Mandeep Wraich <mandeepw@nulogy.com>
2018-10-31 21:37:48 -0400

    Update Hamza now Jassi and Ankita's profiles with new photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,24 +9,25 @@ class PhotosController < ApplicationController
   def ankita
     {
       name: 'ankita',
-      full_name: 'Ankita Lastname',
-      followers: 1390,
-      following: 75,
-      posts: 13,
-      photo_count: 13,
-      likes_range: (1..100).to_a
+      full_name: 'Aankita B',
+      followers: '137k',
+      following: 353,
+      posts: 530,
+      photo_count: 14,
+      likes_range: (5000..10000).to_a
     }
   end
 
-  def hamza
+
+  def jassi
     {
-      full_name: 'Hamza Lastname',
-      name: 'hamza',
-      followers: 24_110,
-      following: 20,
-      posts: 9,
-      photo_count: 9,
-      likes_range: (1..100).to_a
+      full_name: 'Jassi Sandhu',
+      name: 'jassi',
+      followers: 848,
+      following: 153,
+      posts: 216,
+      photo_count: 24,
+      likes_range: (100..250).to_a
     }
   end
 
@@ -37,8 +38,8 @@ class PhotosController < ApplicationController
       followers: '6,983',
       following: 454,
       posts: 521,
-      photo_count: 21,
-      likes_range: (5000..6000).to_a
+      photo_count: 16,
+      likes_range: (500..2000).to_a
     }
   end
 end
